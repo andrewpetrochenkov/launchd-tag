066704ac94424309d4531931bbb0c3df2802f2e3
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:03:04 +0300

    2020:12:04 00:03

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='launchd-tag',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/launchd-tag']
